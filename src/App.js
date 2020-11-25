5c6969e5138f71c9fd12cc49665c7b53d5d69494
Kellen Hawley <hawleykc@gmail.com>
2020-11-24 21:36:46 -0800

    great styles, low low prices

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { createWorker } from 'tesseract.js';
 import boons from './boons';
 import useCopyToClipboard from './useCopyToClipboard';
 
-
 console.log(boons)
 
 function App() {
-  const [ocr, setOcr] = useState('select an image above');
+  const [ocr, setOcr] = useState(undefined);
   const [image, setImage] = useState();
+  const [twitter, setTwitter] = useState();
   const [copy, isCopied] = useCopyToClipboard(1000);
+
+  useEffect(() => {
+    if (twitter) {
+      const script = document.createElement('script');
+      script.src = 'https://platform.twitter.com/widgets.js';
+      document.body.appendChild(script);
+    }
+  }, [twitter]);
 
   useEffect(() => {
     if (image) {
@@ -40,13 +48,33 @@ function App() {
         }}
       />
 
-      <label htmlFor='file' className='box box-interactive'>Upload Security Report</label>
-      {image && <p className='box'>currently selected: {image.name}</p>}
+      {image && <p className='📦'>currently selected: {image.name}</p>}
 
-      <button className='results' onClick={() => copy(ocr)}>
-        {ocr}
-      </button>
+      {
+        ocr &&
+        <button className='results' onClick={() => copy(ocr)}>
+          {ocr}
+        </button>
+      }
+
+      <label htmlFor='file' className='🍑on'>Upload Security Report</label>
       {isCopied && <p className='fadeIn box'>Copied.</p>}
+
+      <marquee>or</marquee>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setTwitter(e.target.username.value);
+      }}>
+        <input className='twitter-input' type='text' name='username' placeholder="Twitter handle..." />
+        <button className='🍑on'>Load Twitter Account</button>
+      </form>
+
+      {twitter && (
+        <div className='twitterStream'>
+          <a className='twitter-timeline' href={`https://twitter.com/${twitter}?ref_src=twsrc%5Etfw`}>{twitter}</a>
+        </div>
+      )}
     </section>
   );
 }
